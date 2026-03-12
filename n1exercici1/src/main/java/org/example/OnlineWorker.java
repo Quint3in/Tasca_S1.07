99c21a4f024b2343531e6922410840f5742ca8f5
JoelQuintana <joelqn95@gmail.com>
2026-03-12 12:53:52 +0100

    feat(n1exercici1): add flat fees to online and onsite salary calculations

--- n1exercici1/src/main/java/org/example/OnlineWorker.java
+++ n1exercici1/src/main/java/org/example/OnlineWorker.java
@@ -2,12 +2,14 @@ package org.example;
 
 public class OnlineWorker extends Worker {
 
+    private final double FLAT_INTERNET_FEE = 30;
+
     public OnlineWorker(String name, String surname, double pricePerHour) {
         super(name, surname, pricePerHour);
     }
 
     @Override
     public double calculateSalary(int hours) {
-        return 0.95 * super.calculateSalary(hours);
+        return (0.95 * super.calculateSalary(hours)) + FLAT_INTERNET_FEE;
     }
 }
--- n1exercici1/src/main/java/org/example/OnsiteWorker.java
+++ n1exercici1/src/main/java/org/example/OnsiteWorker.java
@@ -2,13 +2,14 @@ package org.example;
 
 public class OnsiteWorker extends Worker {
 
+    private static final double FUEL = 50;
 
     public OnsiteWorker(String name, String surname, double pricePerHour) {
         super(name, surname, pricePerHour);
     }
 
     @Override
-    public double calculateSalary(int hours) {
-        return 1.05 * super.calculateSalary(hours);
+    public double calculateSalary(int hoursMonth) {
+        return (1.05 * super.calculateSalary(hoursMonth)) + FUEL;
     }
 }
